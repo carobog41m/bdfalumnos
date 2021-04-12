510b9969bb6c75d42c127dd1a59798cea542d8bf
carobog41m <82359863+carobog41m@users.noreply.github.com>
2021-04-12 00:24:23 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBCgD7revOYd7iuuLa9skGh06uSDGDpRdo",
+    authDomain: "dbalum41m.firebaseapp.com",
+    projectId: "dbalum41m",
+    storageBucket: "dbalum41m.appspot.com",
+    messagingSenderId: "487371510882",
+    appId: "1:487371510882:web:4e4a2a2b29e2ddf5026d3b",
+    measurementId: "G-TLE699YS6B"
+});
